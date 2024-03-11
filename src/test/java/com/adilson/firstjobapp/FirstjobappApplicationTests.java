3ecdbc20505bf98cd830c3949118672a5bfdbd96
adilsontchameia <adilsontchameia@gmail.com>
2024-03-11 18:46:26 +0100

    first commit

---
+++ src/test/java/com/adilson/firstjobapp/FirstjobappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adilson.firstjobapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstjobappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
