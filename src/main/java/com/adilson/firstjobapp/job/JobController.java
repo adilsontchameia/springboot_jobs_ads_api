f35fd1d921e3eb51d670753c443cd65ff0e44657
adilsontchameia <adilsontchameia@gmail.com>
2024-03-12 20:36:31 +0100

    feat: endpoints note

--- src/main/java/com/adilson/firstjobapp/job/JobController.java
+++ src/main/java/com/adilson/firstjobapp/job/JobController.java
@@ -11,10 +11,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class JobController{
 
-    private List<Job> jobs = new ArrayList<>();
+    /* Endpoints and examples below
+    GET /jobs: Get all jobs
+    GET {base_url}/jobs
+
+    GET /jobs/{id}: Get a specific job by ID
+    GET {base_url}/jobs/1
+
+    POST /jobs: Create a new job (request body should contain the job details)
+    POST {base_url}/jobs
+
+    DELETE /jobs/{id}: Delete a specific job by ID
+    DELETE {base_url}/jobs/1
+
+    PUT /jobs/{id}: Update a specific job by ID (request body should contain the job details)
+    PUT {base_url}/jobs/1
+
+    GET /jobs/{id}/company: Get the company associated with a specific job by ID
+    GET {base_url}/jobs/1/company
+     */
+
+
+
+    private final List<Job> jobs = new ArrayList<>();
 
     @GetMapping("/jobs")
     public List<Job> findAll(){
         return jobs;
     }
+
 }
